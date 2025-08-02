24190e1e7a32fe99fa4c4109aef37afdf0de7481
Vadym <innsbruck1999@gmail.com>
2025-08-03 00:37:18 +0100

    modify

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -47,7 +47,7 @@ function addLeadingZero(value) {
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
-  let hour = minute * 60;
+  const hour = minute * 60;
   const day = hour * 24;
 
   const days = Math.floor(ms / day);
